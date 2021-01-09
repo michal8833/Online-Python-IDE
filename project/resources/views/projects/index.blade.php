1f1caf880739d0452f717e26da3b804ad0d24d2c
Michał Marczak <michal.m8833@gmail.com>
2021-01-10 00:34:15 +0100

    Add views/projects directory and projects.index view

---
+++ project/resources/views/projects/index.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <div class="container-fluid mt-3">
+        <div class="container mt-3 pl-3">
+            <div class="alert alert-primary" role="alert">
+                <strong>You're logged in!</strong>
+            </div>
+        </div>
+
+
+        @include('layouts.footers.auth')
+    </div>
+
+@endsection
+
+@push('js')
+    <script src="{{ asset('argon') }}/vendor/chart.js/dist/Chart.min.js"></script>
+    <script src="{{ asset('argon') }}/vendor/chart.js/dist/Chart.extension.js"></script>
+@endpush
