d5315ec59f36ec58c493ec28bc9f4ae191fbeb55
Piotr Sniezek <sniegu703@gmail.com>
2021-01-09 20:11:02 +0100

    Password reset

--- project/routes/web.php
+++ project/routes/web.php
@@ -17,9 +17,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/sss',function (){
-    return view( 'welcome');
-});
+require __DIR__.'/auth.php';
 
 Auth::routes();
 
