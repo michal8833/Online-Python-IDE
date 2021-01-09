4e0b19cfd7d0d363ad95f2b66cbc1a164c1e2575
Piotr Sniezek <sniegu703@gmail.com>
2021-01-09 14:49:15 +0100

    Removed auth.php

--- project/routes/web.php
+++ project/routes/web.php
@@ -21,11 +21,6 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
-require __DIR__.'/auth.php';
-
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-
-
-
-
+Auth::routes();
