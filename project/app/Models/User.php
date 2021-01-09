5e52fde4bdde54ad1f8681790957147a8a33eb58
Filip Sądowicz <sadowicz31@gmail.com>
2021-01-09 12:56:54 +0100

    Adding Project-User 1:N relationship

--- project/app/Models/User.php
+++ project/app/Models/User.php
@@ -7,8 +7,8 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
-{
+class User extends Authenticatable {
+
     use HasFactory, Notifiable;
 
     /**
@@ -40,4 +40,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function projects() {
+
+        return $this->hasMany(Project::class);
+    }
 }
