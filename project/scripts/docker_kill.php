85fead980d278cea98e3209ad692c73c6736c7ef
Tomasz Wojna <twojna@interia.pl>
2021-01-11 00:52:08 +0100

    # Docker configuration + fixed docker run scripts

--- project/scripts/docker_kill.php
+++ project/scripts/docker_kill.php
@@ -6,7 +6,7 @@ $env = get_env();
 
 $containerRunning = shell_exec("if docker ps | grep -q {$env['DOCKER_CONTAINER_NAME']}; then printf 'true'; else printf 'false'; fi");
 if ($containerRunning === 'true') {
-    print "Killed container: ".shell_exec("docker stop $(docker ps -q --filter ancestor={$env['DOCKER_CONTAINER_NAME']})")."\n";
+    print "Killed container: ".shell_exec("docker kill $(docker ps -q --filter ancestor={$env['DOCKER_CONTAINER_NAME']})")."\n";
 } else {
     echo "The python-interpreter container isn't running\n";
 }
