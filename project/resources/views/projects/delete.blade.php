52a9373bd704fdb5091fa1d542766f67fc24d9bd
Filip Sądowicz <sadowicz31@gmail.com>
2021-01-12 20:19:57 +0100

    Adding delete view design

--- project/resources/views/projects/delete.blade.php
+++ project/resources/views/projects/delete.blade.php
@@ -2,7 +2,18 @@
 
 @section('content')
 
-    <h2>{{$project->id}}</h2>
+    <div class="container-fluid mt-3 col-12">
+        <div class="card center" style="width: 36rem;">
+            <div class="card-body text-center">
+                <h4 class="card-title">Delete project</h4>
+                <p class="card-text">Are you sure you want to delete project <span class="font-weight-bold">{{ $project->name }}</span>? All included files will be deleted too.</p>
+                <div>
+                    <a href="#" class="btn btn-primary">Yes</a>
+                    <a href="#" class="btn btn-primary">No</a>
+                </div>
+            </div>
+        </div>
+    </div>
 
     @include('layouts.footers.auth')
 @endsection
